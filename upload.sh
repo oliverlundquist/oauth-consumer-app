115463eb70c00c945f3698986b8501f42eee14e9
oliverlundquist <mail@oliverlundquist.com>
2021-05-21 11:37:28 +0900

    tweaked upload script

--- upload.sh
+++ upload.sh
@@ -1,1 +1,1 @@
-aws s3 sync . s3://mystore-api-auth-app --acl public-read --delete --exclude '.git/*' --exclude '.DS_Store'
+docker run -it -v ~/.aws-mystore:/root/.aws -v "$PWD":/app -w /app oliverlundquist/aws-cli aws s3 sync . s3://mystore-api-auth-app --acl public-read --delete --exclude '.git/*' --exclude '.DS_Store'
